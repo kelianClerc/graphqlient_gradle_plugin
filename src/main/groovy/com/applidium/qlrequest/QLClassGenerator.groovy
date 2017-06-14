d40c745be576f01ea95fc60b4349a777801f4861
Kélian Clerc <kelian.clerc@applidium.com>
2017-06-14 11:27:18 +0200

    Refactor and quick fix ClassGenerator

--- src/main/groovy/com/applidium/qlrequest/QLClassGenerator.groovy
+++ src/main/groovy/com/applidium/qlrequest/QLClassGenerator.groovy
@@ -10,6 +10,8 @@ import com.squareup.javapoet.*
 import javax.lang.model.element.Modifier
 
 class QLClassGenerator {
+
+    private final String PACKAGE = "com.applidium.graphqlient";
     QLQuery qlQuery;
 
     def generateSource(File file, String packageName) {
@@ -49,36 +51,27 @@ class QLClassGenerator {
 
         computeParams(fields, constructor, getterAndSetter, mandatoryFields)
         computeVarsMap(fields, getterAndSetter);
+        boolean areConstructorParam = fields.size() > 0;
+        addTarget(fields, getterAndSetter, packageName, className);
+        addQuery(fields, getterAndSetter, mandatoryFields);
 
-        ClassName targetClassName = ClassName.get(packageName, className + "Response");
-
-        FieldSpec target = FieldSpec
-                .builder(Class.class, "target", Modifier.PUBLIC, Modifier.FINAL)
-                .initializer("\$T.class", targetClassName)
-                .build();
-
-        FieldSpec.Builder queryField = FieldSpec
-                .builder(String.class, "query", Modifier.PRIVATE, Modifier.FINAL);
-        queryField.initializer("\$S", qlQuery.printQuery())
+        ClassName qlRequest = ClassName.get(PACKAGE, "QLRequest");
 
         TypeSpec.Builder query = TypeSpec.classBuilder(className + "Request")
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addField(queryField.build())
-                .addFields(fields)
-                .addField(target)
                 .addMethod(constructor.build())
-                .addMethod(getQuery(mandatoryFields))
-                .addMethods(getterAndSetter);
+                .addFields(fields)
+                .addMethods(getterAndSetter)
+                .addSuperinterface(qlRequest)
 
-        if (fields.size() > 0) {
+        if (areConstructorParam) {
             query.addMethod(emptyConstructor.build());
         }
 
         return query.build();
     }
 
-    private
-    void computeParams(
+    private void computeParams(
             ArrayList<FieldSpec> fields,
             MethodSpec.Builder constructor,
             ArrayList<MethodSpec> getterAndSetter,
@@ -120,7 +113,7 @@ class QLClassGenerator {
     }
 
     MethodSpec generateGetter(ParameterSpec param) {
-        MethodSpec.Builder getter = MethodSpec.methodBuilder("get" + param.name.capitalize())
+        MethodSpec.Builder getter = MethodSpec.methodBuilder(param.name)
                 .addModifiers(Modifier.PUBLIC)
                 .returns(param.type);
 
@@ -154,35 +147,84 @@ class QLClassGenerator {
 
     void computeVarsMap(ArrayList<FieldSpec> fieldSpecs, ArrayList<MethodSpec> methodSpecs) {
 
-        ClassName map = ClassName.get("java.util", "Map");
-        ClassName hashmap = ClassName.get("java.util", "HashMap");
-        ClassName key = ClassName.get("java.lang", "String");
-        ClassName value = ClassName.get("java.lang", "Object");
-        TypeName mapVars = ParameterizedTypeName.get(map, key, value);
+        ClassName stringClass = ClassName.get("java.lang", "String");
+        ClassName stringBuilder = ClassName.get("java.lang", "StringBuilder");
 
-        MethodSpec.Builder getVars = MethodSpec.methodBuilder("getVariables");
+
+        MethodSpec.Builder getVars = MethodSpec.methodBuilder("variables");
         getVars.addModifiers(Modifier.PUBLIC);
-        getVars.returns(Map.class);
-        getVars.addStatement("\$T result = new \$T<>()", mapVars, hashmap)
+        getVars.addAnnotation(Override.class)
+        getVars.returns(stringClass);
+        getVars.addStatement("\$T result = new \$T()", stringBuilder, stringBuilder);
+        getVars.addStatement("result.append(\"{\")");
+
+        int i = 0;
 
         for (FieldSpec fieldSpec : fieldSpecs) {
             getVars.beginControlFlow("if (\$N != null)", fieldSpec.name);
-            getVars.addStatement("result.put(\$S, \$N)", fieldSpec.name, fieldSpec.name);
+            getVars.addStatement("result.append(\"\\\"\")");
+            getVars.addStatement("result.append(\$S)", fieldSpec.name);
+            getVars.addStatement("result.append(\"\\\"\")");
+            getVars.addStatement("result.append(\":\")");
+            getVars.addStatement("result.append(\$N)", fieldSpec.name);
             getVars.endControlFlow();
+            if (i < fieldSpecs.size() - 1) {
+                getVars.addStatement("result.append(\",\")");
+            }
         }
+        getVars.addStatement("result.append(\"}\")");
 
-        getVars.addStatement("return result");
+        getVars.addStatement("return result.toString()");
         methodSpecs.add(getVars.build());
     }
 
-    private MethodSpec getQuery(List<FieldSpec> mandatoryFields) {
-        String packageName = "com.applidium.qlrequest.exceptions"
-        ClassName exception = ClassName.get(packageName, "QLException");
+    private void addTarget(
+            ArrayList<FieldSpec> fieldSpecs,
+            ArrayList<MethodSpec> methodSpecs,
+            String packageName,
+            String className
+    ) {
+        ClassName targetClassName = ClassName.get(packageName, className + "Response");
+        ClassName qlResponse = ClassName.get(PACKAGE, "QLResponseModel");
+        TypeName wildcardClass = ParameterizedTypeName.get(ClassName.get(Class.class), WildcardTypeName.subtypeOf(qlResponse));
+        TypeName targetClass = ParameterizedTypeName.get(ClassName.get(Class.class), targetClassName);
 
-        MethodSpec.Builder statement = MethodSpec.methodBuilder("query").
-                addModifiers(Modifier.PUBLIC)
+        FieldSpec target = FieldSpec
+                .builder(wildcardClass, "target", Modifier.PUBLIC, Modifier.FINAL)
+                .initializer("\$T.class", targetClassName)
+                .build()
+
+        MethodSpec targetMethod = MethodSpec.methodBuilder("target")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
+                .returns(targetClass)
+                .addStatement("return (\$T) \$N", targetClass, target)
+                .build()
+
+        fieldSpecs.add(target)
+        methodSpecs.add(targetMethod)
+    }
+
+    private void addQuery(
+            ArrayList<FieldSpec> fields,
+            ArrayList<MethodSpec> methods,
+            ArrayList<FieldSpec> mandatoryFields
+    ) {
+        FieldSpec.Builder queryField = FieldSpec
+                .builder(String.class, "query", Modifier.PRIVATE, Modifier.FINAL);
+        queryField.initializer("\$S", qlQuery.printQuery())
+        fields.add(queryField.build())
+        methods.add(getQuery(mandatoryFields))
+    }
+
+    private MethodSpec getQuery(List<FieldSpec> mandatoryFields) {
+        ClassName exception = ClassName.get(PACKAGE + ".exceptions", "QLException");
+
+        MethodSpec.Builder statement = MethodSpec.methodBuilder("query")
+                .addModifiers(Modifier.PUBLIC)
+                .addAnnotation(Override.class)
                 .returns(String.class)
-                //.addException(exception);
+                .addException(exception);
 
         for (FieldSpec field : mandatoryFields) {
             statement.beginControlFlow("if(\$N == null)", field.name);
@@ -195,19 +237,24 @@ class QLClassGenerator {
     }
 
     TypeSpec generateResponse(String fileName, String packageName) {
-        MethodSpec.Builder constructor = MethodSpec
-                .constructorBuilder()
-                .addModifiers(Modifier.PUBLIC);
-        MethodSpec.Builder emptyConstructor = MethodSpec
-                .constructorBuilder()
-                .addModifiers(Modifier.PUBLIC);
 
-        createModels(fileName, packageName)
+        return createModels(fileName, packageName)
     }
 
     TypeSpec createModels(String className, String packageName) {
 
-        TypeSpec.Builder queryRespose = TypeSpec.classBuilder(className + "Response").addModifiers(Modifier.PUBLIC);
+
+        ClassName qlResponse = ClassName.get(PACKAGE, "QLResponseModel");
+
+        MethodSpec.Builder emptyConstructor = MethodSpec
+                .constructorBuilder()
+                .addModifiers(Modifier.PUBLIC);
+
+        TypeSpec.Builder queryRespose = TypeSpec
+                .classBuilder(className + "Response")
+                .addModifiers(Modifier.PUBLIC)
+                .addMethod(emptyConstructor.build())
+                .addSuperinterface(qlResponse)
 
         for (QLNode root : qlQuery.getQueryFields()) {
             horizontalTreeReed(root, queryRespose, packageName)
@@ -220,10 +267,11 @@ class QLClassGenerator {
         String packageNameChild = packageName + "." + parent.build().name;
 
         String elementName = qlElement.getAlias() == null ? qlElement.getName() : qlElement.getAlias();
+        ClassName qlModel = ClassName.get(PACKAGE + ".model", "QLModel")
 
         if (qlElement instanceof QLNode) {
             TypeSpec.Builder model = TypeSpec.classBuilder(elementName.capitalize())
-                    //.superclass(QLModel.class)
+                    .addSuperinterface(qlModel)
                     .addModifiers(Modifier.PUBLIC, Modifier.STATIC);
             generateFieldSetterGetter(parent, builderType(packageNameChild, elementName, qlElement.isList()), elementName);
             for (QLElement child : qlElement.getChildren()) {
